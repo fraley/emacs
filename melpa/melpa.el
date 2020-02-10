792adecd27549a76ce6c9b7b75a9726428842d6b
Paul Fraley <paul-online@caltonia.com>
2020-02-10 04:22:55 +0000

    melpa: load a bunch of packages at once, start thinking about use-package

--- melpa/melpa.el
+++ melpa/melpa.el
@@ -1,3 +1,15 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
+
+; (package-install 'flymake-easy)
+; (package-install 'flymake-cursor)
+; (package-install 'json-mode)
+; (package-install 'flymake-json)
+; (package-install 'protobuf-mode)
+; (package-install 'go-mode)
+
+(package-install 'ivy-hydra)
+(package-install 'elpy)
+(package-install 'py-autopep8)
---
+++ melpa/melpa.hot.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-hot" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+; (package-install 'use-package)
