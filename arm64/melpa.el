b21b153a8dbed396183fa01233178fc5fa5a8354
Paul Fraley <paul-online@caltonia.com>
2024-05-31 17:51:25 -0700

    arm64/melpa.el: add gnu-elpa-keyring-update and update existing packages

--- arm64/melpa.el
+++ arm64/melpa.el
@@ -1,8 +1,10 @@
+;;; package --- Summary
 (require 'package)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
+;;; Commentary:
 ; (package-initialize) necessary before require '<some-package-I-installed> works
 ; (example: before (require 'use-package) works, for example
 (package-initialize)
@@ -11,6 +13,7 @@
 
 (package-install 'use-package)
 
+;;; Commentary:
 ; use 'use-package for packages (like flycheck)
 (require 'use-package)
 
@@ -20,13 +23,19 @@
 
 (package-install 'go-mode)
 
+;;; Commentary:
+; https://stackoverflow.com/questions/58202993/emacs-failed-to-verify-signature-archive-contents-sig
+; https://emacs.stackexchange.com/questions/233/how-to-proceed-on-package-el-signature-check-failure
+(package-install 'gnu-elpa-keyring-update)
+
+;;; Commentary:
 ; When looking for support for things like yaml, search google for yaml-mode.
 ; Compare that with what you get when you do M-x:package-list-packages, search
 ; for yaml-mode in that buffer and hit <enter> on yaml-mode.  In that example,
 ; you will see the same result: https://github.com/yoshiki/yaml-mode.
 ;
 ; When adding new packages, do a dirdff beforehand
-; 1) ~/bin/dirdiff ~/.emacs.d # see dirdiff above
+; 1) ~/bin/dirdiff ~/.emacs.d # reset dirdiff list
 ; 2) add package to the list below, for example: (package-install 'protobuf-mode)
 ; 3) M-x: load-file # ~/emacs/arm64/melpa.el
 ; 4) ~/bin/dirdiff ~/.emacs.d # again, see new packages below
@@ -45,6 +54,14 @@
 ;    /Users/pfraley/.emacs.d/elpa/use-package-20230426.2324/doclicense.info
 ;    /Users/pfraley/.emacs.d/elpa/use-package-20230426.2324/docstyle.info
 ;
+; To update already installed packages:
+; https://stackoverflow.com/questions/14836958/updating-packages-in-emacs
+; 1) ~/bin/dirdiff ~/.emacs.d # reset dirdiff list
+; 2) (package-list-packages)
+; 3) U - runs package-menu-mark-upgrades
+; 4) x - runs package-menu-execute
+; 5) ~/bin/dirdiff ~/.emacs.d # again, see updates to existing packages
+
 (package-install 'protobuf-mode)
 (package-install 'json-mode)
 (package-install 'yaml-mode)
